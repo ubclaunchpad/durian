9ad5da181d803aea7406006094efc8a615fc4c19
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2018-05-28 23:42:19 -0700

    Updated test cases to expect VM

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,8 @@
 #include <gtest/gtest.h>
 
-#include <ExampleClass.h>
+
 #include <Lexer.h>
 
-TEST(TestExClass, TestRetOne) {
-    EXPECT_EQ(ExampleClass::returnOne(), 1);
-}
 
 TEST(TestLexer, TestSingleComma) {
     Lexer lexer(",");
