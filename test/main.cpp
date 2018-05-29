db8ef01137dfbc1f794eb1c6b420d0e9ef521653
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2018-05-28 23:42:19 -0700

    Updated test cases to expect VM

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
 
-#include <ExampleClass.h>
+#include <VM.h>
 
 TEST(TestExClass, TestRetOne) {
-    EXPECT_EQ(ExampleClass::returnOne(), 1);
+    char dummy_bytecodes[] = {0x00};
+    EXPECT_EQ(VM(dummy_bytecodes).run(), 0);
 }
