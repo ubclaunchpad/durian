cd7a7af3fa4ba147a6c3079ad551df01dc548475
Thiabaud Engelbrecht <thiabaud.engelbrecht@gmail.com>
2018-06-02 11:57:41 -0700

    changed to raw enum

--- src/include/opcode.h
+++ src/include/opcode.h
@@ -1,15 +1,19 @@
 #ifndef DURIAN_OPCODE_H
 #define DURIAN_OPCODE_H
 
-enum struct Opcode : unsigned char {
-  NOP     = 0x00,  // pass
-  ADD     = 0x60,  // add top two elements on stack, push result back onto stack.
-  SUB     = 0x61,  // subtract top two elements on stack, push result back onto stack.
-  MUL     = 0x62,  // multiply top two elements on stack, push result back onto stack.
-  FDIV    = 0x64,  // divide top two elements on stack, push result back onto stack as a double.
-  NEG     = 0x67,  // negate top element on stack, push result back onto stack.
-  CNCT    = 0x6B,  // concatenate top two elements on stack, push result back onto stack.
-  HALT    = 0xF0   // halt
-};
-
+namespace Opcode {
+    enum : unsigned char {
+      NOP       = 0x00,  // pass
+      ICONST_0  = 0x04,
+      ICONST_1  = 0x05,
+      ADD       = 0x60,  // add top two elements on stack, push result back onto stack.
+      SUB       = 0x61,  // subtract top two elements on stack, push result back onto stack.
+      MUL       = 0x62,  // multiply top two elements on stack, push result back onto stack.
+      FDIV      = 0x64,  // divide top two elements on stack, push result back onto stack as a double.
+      NEG       = 0x67,  // negate top element on stack, push result back onto stack.
+      CNCT      = 0x6B,  // concatenate top two elements on stack, push result back onto stack.
+      HALT      = 0xF0,  // halt
+      PRINT     = 0xFF
+    };
+}
 #endif
