4361017a3cc1c64346538dc0bceeecb24a12bee8
Thiabaud Engelbrecht <thiabaud.engelbrecht@gmail.com>
2018-06-09 13:14:09 -0700

    added DUP and BR_F opcodes

--- src/include/opcode.h
+++ src/include/opcode.h
@@ -8,6 +8,7 @@ namespace Opcode {
       ICONST_1  = 0x05,  // push literal int(1) onto stack.
       BCONST_F  = 0x0E,  // push literal bool(false) onto stack.
       BCONST_T  = 0x0F,  // push literal bool(true) onto stack.
+      DUP       = 0x58,  // duplicate top of stack.
       ADD       = 0x60,  // add top two elements on stack, push result back onto stack.
       SUB       = 0x61,  // subtract top two elements on stack, push result back onto stack.
       MUL       = 0x62,  // multiply top two elements on stack, push result back onto stack.
@@ -16,6 +17,7 @@ namespace Opcode {
       NOT       = 0x69,  // negates a boolean on top of the stack.
       CNCT      = 0x6B,  // concatenate top two elements on stack, push result back onto stack.
       EQ        = 0x74,  // checks if the top two elements of the stack are equal.
+      BR_F      = 0x93,  // pops the top of the stack, then branches if it is false. (takes next 4 bytes as jump length)
       HALT      = 0xF0,  // halt
       PRINT     = 0xFF   // print top of stack with newline, discarding top of stack.
     };
