51bf2ded99cabe9eb0801542d5a2987f11381d3d
Russell Blickhan <rwblickhan@gmail.com>
2018-06-23 18:53:32 -0700

    Fix missing 'virtual' on Visitor interface methods

--- src/include/Visitor.h
+++ src/include/Visitor.h
@@ -5,24 +5,24 @@
 
 class Visitor {
 private:
-    void visit(AST::AssignStmt *node) = 0;
-    void visit(AST::BinaryExpr *node) = 0;
-    void visit(AST::BlockStmt *node) = 0;
-    void visit(AST::BooleanLit *node) = 0;
-    void visit(AST::BreakStmt *node) = 0;
-    void visit(AST::ErrStmt *node) = 0;
-    void visit(AST::ExprStmt *node) = 0;
-    void visit(AST::FloatLit *node) = 0;
-    void visit(AST::FnCall *node) = 0;
-    void visit(AST::FnDecl *node) = 0;
-    void visit(AST::Identifier *node) = 0;
-    void visit(AST::IfStmt *node) = 0;
-    void visit(AST::LetStmt *node) = 0;
-    void visit(AST::NextStmt *node) = 0;
-    void visit(AST::PrintStmt *node) = 0;
-    void visit(AST::ReturnStmt *node) = 0;
-    void visit(AST::ScanStmt *node) = 0;
-    void  visit(AST::StringLit *node) = 0;
-    void visit(AST::UnaryExpr *node) = 0;
-    void visit(AST::WhileStmt *node) = 0;
+    virtual void visit(AST::AssignStmt *node) = 0;
+    virtual void visit(AST::BinaryExpr *node) = 0;
+    virtual void visit(AST::BlockStmt *node) = 0;
+    virtual void visit(AST::BooleanLit *node) = 0;
+    virtual void visit(AST::BreakStmt *node) = 0;
+    virtual void visit(AST::ErrStmt *node) = 0;
+    virtual void visit(AST::ExprStmt *node) = 0;
+    virtual void visit(AST::FloatLit *node) = 0;
+    virtual void visit(AST::FnCall *node) = 0;
+    virtual void visit(AST::FnDecl *node) = 0;
+    virtual void visit(AST::Identifier *node) = 0;
+    virtual void visit(AST::IfStmt *node) = 0;
+    virtual void visit(AST::LetStmt *node) = 0;
+    virtual void visit(AST::NextStmt *node) = 0;
+    virtual void visit(AST::PrintStmt *node) = 0;
+    virtual void visit(AST::ReturnStmt *node) = 0;
+    virtual void visit(AST::ScanStmt *node) = 0;
+    virtual void visit(AST::StringLit *node) = 0;
+    virtual void visit(AST::UnaryExpr *node) = 0;
+    virtual void visit(AST::WhileStmt *node) = 0;
 };
