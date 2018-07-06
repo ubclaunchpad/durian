fa115c10df17a826951357041ef797df02b0c51e
Russell Blickhan <rwblickhan@gmail.com>
2018-07-05 21:43:31 -0700

    Cleanup VM declaration to fit other classes

--- src/include/VM.h
+++ src/include/VM.h
@@ -3,19 +3,19 @@
 #include <DurianObject.h>
 
 class VM {
-    public:
-        VM(unsigned char *);
-        ~VM() = default;
-        int run();
-    private:
-        int m_pc;                  // Program Counter
-        int m_sp;                  // Stack Pointer
-        unsigned char *m_code;     // Pointer to bytecode
-        static const int STACK_SIZE = 256;
-        DurianObject stack[STACK_SIZE];      // Stack
-        void push(DurianObject);
-        DurianObject pop();
-        unsigned char nextBytecode();
-        void typeError(const char *operand, DurianObject a);
-        void typeError(const char *operand, DurianObject a, DurianObject b);
+    int m_pc;                  // Program Counter
+    int m_sp;                  // Stack Pointer
+    unsigned char *m_code;     // Pointer to bytecode
+    static const int STACK_SIZE = 256;
+    DurianObject stack[STACK_SIZE];      // Stack
+public:
+    VM(unsigned char *);
+    ~VM() = default;
+    int run();
+private:
+    void push(DurianObject);
+    DurianObject pop();
+    unsigned char nextBytecode();
+    void typeError(const char *operand, DurianObject a);
+    void typeError(const char *operand, DurianObject a, DurianObject b);
 };
