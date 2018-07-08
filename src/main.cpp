ed393d5ff4628b3d3761eca402a9438da2eab0c6
Russell Blickhan <rwblickhan@gmail.com>
2018-07-07 22:26:03 -0700

    Create durian executable and implement pretty printing in it

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,130 @@
-#include <iostream>
+#include <Lexer.h>
+#include <Parser.h>
+#include <PrettyPrinter.h>
 
-int main() {
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+// based on https://stackoverflow.com/a/868894
+class ArgParser {
+    std::vector<std::string> args;
+public:
+    enum struct Mode{
+        PrettyPrint,
+        CompileOnly,
+        InterpretOnly,
+        None
+    } mode;
+    std::pair<bool, std::string> optionalFilepath;
+    ArgParser(int argc, char** argv)
+        : mode(Mode::None)
+        , optionalFilepath() {
+        for (int i = 0; i < argc; i++) {
+            args.emplace_back(argv[i]);
+        }
+        if (optionExists("-h") || optionExists("--help")) {
+            printHelpAndExit(0);
+        }
+        setModeIfExistsFlag("-p", Mode::PrettyPrint);
+        setModeIfExistsFlag("-c", Mode::CompileOnly);
+        setModeIfExistsFlag("-b", Mode::InterpretOnly);
+        if (args.back() != "-p" &&
+                args.back() != "-c" &&
+                args.back() != "-b" &&
+                args.back() != args.front()) {
+            optionalFilepath.first = true;
+            optionalFilepath.second = args.back();
+        } else {
+            optionalFilepath.first = false;
+            optionalFilepath.second = "";
+        }
+    }
+private:
+    bool optionExists(const std::string& option) {
+        return std::find(args.begin(), args.end(), option) != args.end();
+    }
+    void setModeIfExistsFlag(const std::string& flag, Mode mode) {
+        if (optionExists(flag)) {
+            if (this->mode != Mode::None) {
+                std::cout << "Cannot specify multiple modes!" << std::endl;
+                printHelpAndExit(1);
+            }
+            this->mode = mode;
+        }
+    }
+    [[noreturn]] void printHelpAndExit(int exitCode) {
+        std::cout << "Optional command line arguments are: " << std::endl;
+        std::cout << " -p [source]: Pretty print Durian source code." << std::endl;
+        std::cout << " -c [source]: Produce bytecode but do not execute it." << std::endl;
+        std::cout << " -b [source]: Execute the given bytecode." << std::endl;
+        std::cout << "Each command optionally takes a path to source code or bytecode; if not provided, "
+                "a REPL will be launched." << std::endl;
+        exit(exitCode);
+    }
+};
+
+int prettyPrint(std::pair<bool, std::string> optionalFilepath) {
+    std::string input;
+    if (optionalFilepath.first) {
+        std::ifstream file(optionalFilepath.second);
+        file >> input;
+    } else {
+        // TODO make this more interactive
+        // run "REPL"
+        std::cout << "Input some code! Type `fin` when you're done." << std::endl;
+        bool fin = false;
+        while (!fin) {
+            std::string temp;
+            std::getline(std::cin, temp);
+            if (temp == "fin") {
+                fin = true;
+            } else {
+                input += temp;
+                input += "\n";
+            }
+        }
+    }
+    Parser parser {Lexer(input)};
+    PrettyPrinter pp;
+    bool done = false;
+    while (!done) {
+        auto ast = parser.parse();
+        if (ast == nullptr) {
+            done = true;
+        } else {
+            ast->accept(&pp);
+        }
+    }
     return 0;
 }
+
+int compile(std::pair<bool, std::string> optionalFilepath) {
+    // TODO
+    return 0;
+}
+
+int interpretBytecode(std::pair<bool, std::string> optionalFilepath) {
+    // TODO
+    return 0;
+}
+
+int execute(std::pair<bool, std::string> optionalFilepath) {
+    // TODO
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    ArgParser argParser(argc, argv);
+    switch (argParser.mode) {
+        case ArgParser::Mode::PrettyPrint:
+            return prettyPrint(argParser.optionalFilepath);
+        case ArgParser::Mode::CompileOnly:
+            return compile(argParser.optionalFilepath);
+        case ArgParser::Mode::InterpretOnly:
+            return interpretBytecode(argParser.optionalFilepath);
+        default:
+            return execute(argParser.optionalFilepath);
+    }
+}
+
