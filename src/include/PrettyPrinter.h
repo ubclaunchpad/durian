fa2b375fa6280d71138d5c614d6a47efc3f6a6a6
Russell Blickhan <rwblickhan@gmail.com>
2018-08-11 14:53:01 -0700

    Remove unnecessary virtual specifiers in Visitor subclasses

--- src/include/PrettyPrinter.h
+++ src/include/PrettyPrinter.h
@@ -7,26 +7,26 @@
 class PrettyPrinter : public Visitor {
 public:
     PrettyPrinter() = default;
-    virtual void visit(AST::AssignStmt *node) override;
-    virtual void visit(AST::BinaryExpr *node) override;
-    virtual void visit(AST::BlockStmt *node) override;
-    virtual void visit(AST::BooleanLit *node) override;
-    virtual void visit(AST::BreakStmt *node) override;
-    virtual void visit(AST::ErrStmt *node) override;
-    virtual void visit(AST::ExprStmt *node) override;
-    virtual void visit(AST::FloatLit *node) override;
-    virtual void visit(AST::FnCall *node) override;
-    virtual void visit(AST::FnDecl *node) override;
-    virtual void visit(AST::Identifier *node) override;
-    virtual void visit(AST::IfStmt *node) override;
-    virtual void visit(AST::IntegerLit *node) override;
-    virtual void visit(AST::LetStmt *node) override;
-    virtual void visit(AST::NextStmt *node) override;
-    virtual void visit(AST::PrintStmt *node) override;
-    virtual void visit(AST::ReturnStmt *node) override;
-    virtual void visit(AST::ScanStmt *node) override;
-    virtual void visit(AST::StringLit *node) override;
-    virtual void visit(AST::UnaryExpr *node) override;
-    virtual void visit(AST::WhileStmt *node) override;
+    void visit(AST::AssignStmt *node) override;
+    void visit(AST::BinaryExpr *node) override;
+    void visit(AST::BlockStmt *node) override;
+    void visit(AST::BooleanLit *node) override;
+    void visit(AST::BreakStmt *node) override;
+    void visit(AST::ErrStmt *node) override;
+    void visit(AST::ExprStmt *node) override;
+    void visit(AST::FloatLit *node) override;
+    void visit(AST::FnCall *node) override;
+    void visit(AST::FnDecl *node) override;
+    void visit(AST::Identifier *node) override;
+    void visit(AST::IfStmt *node) override;
+    void visit(AST::IntegerLit *node) override;
+    void visit(AST::LetStmt *node) override;
+    void visit(AST::NextStmt *node) override;
+    void visit(AST::PrintStmt *node) override;
+    void visit(AST::ReturnStmt *node) override;
+    void visit(AST::ScanStmt *node) override;
+    void visit(AST::StringLit *node) override;
+    void visit(AST::UnaryExpr *node) override;
+    void visit(AST::WhileStmt *node) override;
 };
 
